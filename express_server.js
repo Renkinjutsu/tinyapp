d2210473bf93403388c2f965812a8b4f9922ce07
Lucas C <lucas.cheng12@gmail.com>
2019-05-07 18:03:19 +0000

    ejs setup

--- express_server.js
+++ express_server.js
@@ -1,14 +1,32 @@
 var express = require("express");
 var app = express();
 var PORT = 8080; // default port 8080
+app.set('view engine', 'ejs'); //set the view engine to ejs
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+// index page
+app.get('/', function(req, res) {
+  var drinks = [
+      { name: 'Bloody Mary', drunkness: 3 },
+      { name: 'Martini', drunkness: 5 },
+      { name: 'Scotch', drunkness: 10 }
+  ];
+  var tagline = "Any code of your own that you haven't looked at for six or more months might as well have been written by someone else.";
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
+  res.render('pages/index', {
+      drinks: drinks,
+      tagline: tagline
+  });
+});
+
+
+
+// about page
+app.get('/about', function(req, res) {
+    res.render('pages/about');
 });
 
 app.get("/urls.json", (req, res) => {
