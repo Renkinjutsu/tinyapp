6b5b548f3f0b5ad80e72d5adcf410c4990826284
Lucas C <lucas.cheng12@gmail.com>
2019-05-08 19:19:29 +0000

    edit added, cleaned up code

--- express_server.js
+++ express_server.js
@@ -10,7 +10,8 @@ var urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-function generateRandomString() {
+function generateRandomString() 
+{
   let random = '';
   for (let i of [1,2,3,4,5,6]) {
     let num = Math.floor(Math.random() * 99999999)
@@ -23,16 +24,19 @@ function generateRandomString() {
 
 
 // index page
-app.get('/', function(req, res) {
-  
-  var drinks = [
+app.get('/', function(req, res) 
+{
+  var drinks = 
+  [
       { name: 'Bloody Mary', drunkness: 3 },
       { name: 'Martini', drunkness: 5 },
       { name: 'Scotch', drunkness: 10 }
   ];
+
   var tagline = "Any code of your own that you haven't looked at for six or more months might as well have been written by someone else.";
 
-  res.render('pages/index', {
+  res.render('pages/index', 
+  {
       drinks: drinks,
       tagline: tagline
   })
@@ -40,55 +44,68 @@ app.get('/', function(req, res) {
 
 
 // about page
-app.get('/about', function(req, res) {
+app.get('/about', function(req, res) 
+{
     res.render('pages/about');
 });
 
 // url index
-app.get("/urls", (req, res) => {
+app.get("/urls", (req, res) => 
+{
   let templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
 });
 
-app.post('/urls', (req, res) => {
- 
-  console.log(req.statusCode)
+app.post('/urls', (req, res) => 
+{
   let newShortUrl = generateRandomString()
   if (req.body) {
     urlDatabase[newShortUrl] = req.body.longURL
   }
-  console.log(urlDatabase);
   res.redirect(`/urls/${newShortUrl}`)
 })
 
-// url shortener
-app.get('/urls/new', (req, res) => {
+// create new short url 
+app.get('/urls/new', (req, res) => 
+{
   res.render('urls_new');
 });
 
-app.get("/u/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
-  // res.render("urls_show", templateVars);
+app.get("/u/:shortURL", (req, res) => 
+{
   res.redirect(urlDatabase[req.params.shortURL])
 });
 
 // Deleting using short url
-app.post('/urls/:shortURL/delete', (req, res) => {
+app.post('/urls/:shortURL/delete', (req, res) => 
+{
   delete urlDatabase[req.params.shortURL]
   res.redirect('/urls')
 });
 
 // url/shortURL 
-app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+app.post('/urls/:shortURL', (req, res) => {
+  const id = req.params.shortURL;
+  const newLongURL = req.body.newURL;
+  urlDatabase[id] = newLongURL; 
+  res.redirect('/urls');
+});
+
+app.get("/urls/:shortURL", (req, res) => 
+{
+  const short = req.params.shortURL
+  const long = urlDatabase[req.params.shortURL]
+  const templateVars = { shortURL: short, longURL: long};
   res.render("urls_show", templateVars);
 });
 
 // hello demo
-app.get("/hello", (req, res) => {
+app.get("/hello", (req, res) => 
+{
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, () => 
+{
   console.log(`Example app listening on port ${PORT}!`);
 });
